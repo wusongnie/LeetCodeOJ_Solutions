f150f76624e5f5e0f5c8e307df072e04d241893c
Song0811 <wusongnie@126.com>
2017-06-22 16:57:00 +0800

    Create 100_Same Tree.swift

---
+++ 100_Same Tree.swift
@@ -0,0 +1,34 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public var val: Int
+ *     public var left: TreeNode?
+ *     public var right: TreeNode?
+ *     public init(_ val: Int) {
+ *         self.val = val
+ *         self.left = nil
+ *         self.right = nil
+ *     }
+ * }
+ */
+class Solution {
+    var k1=[Int]()
+    var k2=[Int]()
+    func isSameTree(_ p: TreeNode?, _ q: TreeNode?) -> Bool {
+        if(p==nil&&q==nil)
+        {
+            return true
+        }
+        if(p==nil||q==nil){
+            return false
+        }
+        
+        if(p!.val != q!.val){
+            return false
+        }
+        else{
+            return isSameTree(p!.left,q!.left)&&isSameTree(p!.right,q!.right)
+        }
+        return true
+    }
+}
