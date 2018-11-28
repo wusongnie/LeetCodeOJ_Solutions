d603fd99b9c821873d896be48dc6d05be1d0a27a
songnieWu <wusongnie@126.com>
2018-11-27 22:21:09 -0500

    Create 589_N-ary Tree Preorder Traversal.cpp

---
+++ 589_N-ary Tree Preorder Traversal.cpp
@@ -0,0 +1,32 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> children;
+
+    Node() {}
+
+    Node(int _val, vector<Node*> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+class Solution {
+public:
+    vector<int> res;
+    vector<int> preorder(Node* root) {
+        helper(root);
+        return res;
+    }
+   void helper(Node* root){
+       if(!root)
+           return;
+       res.push_back(root->val);
+       for(Node* n: root->children){
+           helper(n);
+       }
+       return;
+    }
+};
