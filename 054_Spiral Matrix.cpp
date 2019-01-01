5b5527ab5fcd653a4314d7012df691b29b0db29c
songnieWu <wusongnie@126.com>
2018-12-31 22:43:04 -0500

    Create 054_Spiral Matrix.cpp

---
+++ 054_Spiral Matrix.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    vector<int> spiralOrder(vector<vector<int>>& matrix) {
+        vector<int> res;
+        if(matrix.size() <= 0 || matrix[0].size() <= 0)
+            return res;
+        int rowBegin = 0;
+        int rowEnd = matrix.size() - 1;
+        int colBegin = 0;
+        int colEnd = matrix[0].size() - 1;
+        
+        while(rowBegin <= rowEnd && colBegin <= colEnd){
+            // Traverse Right
+            for (int j = colBegin; j <= colEnd; j ++) {
+                res.push_back(matrix[rowBegin][j]);
+            }
+            rowBegin++;
+            
+            // Traverse Down
+            for (int j = rowBegin; j <= rowEnd; j ++) {
+                res.push_back(matrix[j][colEnd]);
+            }
+            colEnd--;
+            
+            if (rowBegin <= rowEnd) {
+                // Traverse Left
+                for (int j = colEnd; j >= colBegin; j --) {
+                    res.push_back(matrix[rowEnd][j]);
+                }
+            }
+            rowEnd--;
+            
+            if (colBegin <= colEnd) {
+                // Traver Up
+                for (int j = rowEnd; j >= rowBegin; j --) {
+                    res.push_back(matrix[j][colBegin]);
+                }
+            }
+            colBegin ++;
+        }
+        return res;
+    }
+};
