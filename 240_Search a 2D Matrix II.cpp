2146dda5b315f9104d1427cb70cf9197cadb8f08
songnieWu <wusongnie@126.com>
2019-05-10 13:34:52 -0700

    Create 240_Search a 2D Matrix II.cpp

---
+++ 240_Search a 2D Matrix II.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        if(matrix.size() <= 0)
+            return false;
+        if(matrix[0].size() <= 0)
+            return false;
+        int i = 0;
+        int j = matrix[0].size() - 1;
+        while(j >= 0 && i <= matrix.size() - 1){
+            if(matrix[i][j] == target){
+                return true;
+            }
+            if(matrix[i][j] > target){
+                j--;
+            }else{
+                i++;
+            }
+        }
+        
+        return false;
+    }
+};
