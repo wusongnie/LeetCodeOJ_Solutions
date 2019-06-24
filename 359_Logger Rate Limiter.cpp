ee94c21588d91e59dc87f2d15cd10ae7d6b5fd1f
songnieWu <wusongnie@126.com>
2019-06-23 18:40:15 -0700

    Create 359_Logger Rate Limiter.cpp

---
+++ 359_Logger Rate Limiter.cpp
@@ -0,0 +1,31 @@
+class Logger {
+public:
+    unordered_map<string, int> map;
+    /** Initialize your data structure here. */
+    Logger() {
+        
+    }
+    
+    /** Returns true if the message should be printed in the given timestamp, otherwise returns false.
+        If this method returns false, the message will not be printed.
+        The timestamp is in seconds granularity. */
+    bool shouldPrintMessage(int timestamp, string message) {
+        if(map.find(message) == map.end()){
+            map[message] = timestamp;
+            return true;
+        }else{
+            if(timestamp - map[message] >= 10){
+                map[message] = timestamp;
+                return true;
+            }else{
+                return false;
+            }
+        }
+    }
+};
+
+/**
+ * Your Logger object will be instantiated and called as such:
+ * Logger* obj = new Logger();
+ * bool param_1 = obj->shouldPrintMessage(timestamp,message);
+ */
