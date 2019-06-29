9a340c911634b6d179b4862e376d38aa3611048e
songnieWu <wusongnie@126.com>
2019-06-28 22:07:31 -0700

    Create 693_Binary Number with Alternating Bits.cpp

---
+++ 693_Binary Number with Alternating Bits.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool hasAlternatingBits(int n) {
+        int curr = n % 2;
+        n /= 2;
+        while (n > 0) {
+            if (curr == n % 2) return false;
+            curr = n % 2;
+            n /= 2;
+        }
+        return true;
+    }
+};
