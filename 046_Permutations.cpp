5135c6c479ea468754c5cfa1f94156c5fb06cf85
songnieWu <wusongnie@126.com>
2019-07-04 20:50:37 -0700

    Update 046_Permutations.cpp

--- 046_Permutations.cpp
+++ 046_Permutations.cpp
@@ -1,11 +1,21 @@
 class Solution {
 public:
     vector<vector<int>> permute(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
         vector<vector<int>> res;
-        do{
-            res.push_back(nums);
-        }while(next_permutation(nums.begin(),nums.end()));
+        perm(0, res, nums);
         return res;
     }
+    
+    void perm(int j, vector<vector<int>>& res, vector<int>& nums){
+        if(j >= nums.size()){
+            res.push_back(nums);
+        }
+        
+        for(int i = j; i < nums.size(); ++i){
+            swap(nums[i], nums[j]);
+            perm(j + 1, res, nums);
+            swap(nums[i], nums[j]);
+        }
+    }
+    
 };
